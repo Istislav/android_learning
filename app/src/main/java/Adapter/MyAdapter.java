e11cbe29093fad244ab24bffe5954db0a17a498d
cyber <cyber.unit@gmail.com>
2020-06-11 13:00:32 +0300

    RecycleView using - adapter-item clickable

--- app/src/main/java/Adapter/MyAdapter.java
+++ app/src/main/java/Adapter/MyAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -42,15 +43,24 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
        return listItems.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         public TextView name;
         public TextView description;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
+            itemView.setOnClickListener(this);
             name = (TextView) itemView.findViewById(R.id.list_item_title);
             description = (TextView) itemView.findViewById(R.id.list_item_description);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int position = getAdapterPosition();
+            ListItem item = listItems.get(position);
+
+            Toast.makeText(context, item.getName(), Toast.LENGTH_SHORT).show();
         }
     }
 }
